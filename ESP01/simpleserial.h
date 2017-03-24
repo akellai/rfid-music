ecfca1d4449967687a7621fca58cd3c6f7cfd9d6
ab <ab@AB-HP>
2017-03-24 02:49:14 +0100

    updated protocol description

--- ESP01/simpleserial.h
+++ ESP01/simpleserial.h
@@ -1,18 +1,20 @@
 // simpleserial.h
 /*
-MAGIC:CMD:X:[param]
+/*
+MAGIC:CMD:X[:param]
 u - status is up. NOTE: this is the only command that can have garbage in front of it. Any other command should in the spec or considered a failure
 p - ping (expects 'u' in return)
 i - Init WiFi using web (force) autoconfig
 c - connect WiFi
 r - reconnect WiFi (fast)
+q - quit
 t:id - transmit ID to pimusic (t:XXYYZZFF)
 b:xx - transmit battery readings (b:XXXXXX), XXXXXX is ascii reading
 
 example:
-< MAGIC:u:		// up
+< MAGIC:u		// up
 > MAGIC:t:00000000	// transmit id 00000000
-< MAGIC:1:		// success; 0 - failure
+< MAGIC:d		// u - success; d - failure
 */
 
 #ifndef _SIMPLESERIAL_h
